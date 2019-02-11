ffaa3f30f84253661f294cf240900331593c08c9
Ervinne Sodusta <ervinne.sodusta@nuworks.ph>
2019-02-11 13:11:21 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo PHP_EOL. 'Hello, I\'m the server behind nginx.' . PHP_EOL;
+
+echo PHP_EOL. 'If you\'re reading this, it means you passed security validation. Congratulations!' . PHP_EOL;
